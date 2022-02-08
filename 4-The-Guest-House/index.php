7494e150e78500b436f5675b262418d810a83372
arungria <arucucc@gmail.com>
2022-02-08 22:18:11 +0100

    Add files via upload

---
+++ 4-The-Guest-House/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- #BeginTemplate "../../../../../../private/Templates/DWT/normal.dwt" -->
+<head>
+<meta charset="utf-8">
+
+<!-- #BeginEditable "doctitle" -->
+<title>An Eerie Visit to Canterville Chase</title>
+<meta name="keywords" content="English, literature, canterville, ghost, mansion, manor, Oscar Wilde, adventure, game, gamification, lesson, reader, secondary education">
+<meta name="description" content="Help Virginia get her memories back in this activity based on the short story The Canterville Ghost by Oscar Wilde.">
+<!-- #EndEditable -->
+
+<link href="https://studies.cat/static/webfonts/arufont/stylesheet.css" rel="stylesheet" type="text/css">
+<?php include $_SERVER['DOCUMENT_ROOT'].'/apps/get/en/head.php';?>
+
+
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/apps/get/en/header.php';?>
+<div id="wrapper">
+  <div id="site">
+    <div id="content">
+      <!-- #BeginEditable "article" -->
+		<div id="overflowx">
+			
+		</div>
+    <!-- #EndEditable -->
+    </div>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/apps/get/en/footer.php';?>
+  </div>
+</div>
+</body>
+<!-- #EndTemplate -->
+</html>
